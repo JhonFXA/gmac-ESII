9331662db5fdfdb3479afcb656d32b59c89be9e8
Davi Almeida <Davi.al1411@gmail.com>
2025-10-23 20:21:15 -0300

    feat:Primeiro Commit criando projeto

---
+++ apigmac/src/main/java/com/example/apigmac/ApigmacApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigmac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigmacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigmacApplication.class, args);
+	}
+
+}
