fe7f469a36d4fb34c1adaabfc35855e7ff8fc4ac
Igor Seara <igor.seara04@gmail.com>
2026-01-14 22:42:56 -0300

    Implementação de configuração de conexão com AWS S3

--- apigmac/src/main/java/com/example/apigmac/ApigmacApplication.java
+++ apigmac/src/main/java/com/example/apigmac/ApigmacApplication.java
@@ -1,7 +1,10 @@
 package com.example.apigmac;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootApplication
 public class ApigmacApplication {
@@ -9,4 +12,13 @@ public class ApigmacApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApigmacApplication.class, args);
 	}
+
+//	@Bean
+//	CommandLineRunner testarS3(S3Client s3Client) {
+//		return args -> {
+//			s3Client.listBuckets().buckets()
+//					.forEach(b -> System.out.println(b.name()));
+//		};
+//	}
+
 }
