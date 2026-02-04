3ae9c1ed81bd7e43d1305511442c89bdabc8b960
Igor Seara <igor.seara04@gmail.com>
2026-02-03 23:33:16 -0300

    fix: ajustar atualização de perfil de usuário

--- apigmac/src/main/java/com/example/apigmac/repositorios/RepositorioAdm.java
+++ apigmac/src/main/java/com/example/apigmac/repositorios/RepositorioAdm.java
@@ -1,9 +1,11 @@
 package com.example.apigmac.repositorios;
 
 import com.example.apigmac.entidades.Administrador;
+import com.example.apigmac.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface RepositorioAdm extends JpaRepository<Administrador, UUID> {
+    Administrador findByUsuarioId(UUID usuarioId);
 }
